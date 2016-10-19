611a88059728ea3d5a67e34e6f0919cc1fb94e16
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-19 21:13:22 +0200

    fixed fatal bug

--- is.js
+++ is.js
@@ -3,13 +3,14 @@ var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 
 var interpreter = require('./core.js');
-var packageJson = fs.readFileSync('./package.json', 'utf8');
-var packageP = JSON.parse(packageJson);
+
 if(argv.f){
   var program = fs.readFileSync(argv.f, 'utf8');
   console.log(program);
   interpreter.run(program);
 }
 if(argv.v){
+  var packageJson = fs.readFileSync('./package.json', 'utf8');
+  var packageP = JSON.parse(packageJson);
   console.log('PirateLang: '+packageP.version);
 }
