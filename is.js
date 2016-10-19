103ae002ef4f9bbf8668fd64e7e8ecbe1baa7f07
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-19 18:26:14 +0200

    Added some new commands

--- is.js
+++ is.js
@@ -2,7 +2,13 @@
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 var interpreter = require('./core.js');
-var program = fs.readFileSync(argv.f, 'utf8');
-console.log(program);
-
-interpreter.run(program);
+var packageJson = fs.readFileSync('./package.json', 'utf8');
+var packageP = JSON.parse(packageJson);
+if(argv.f){
+  var program = fs.readFileSync(argv.f, 'utf8');
+  console.log(program);
+  interpreter.run(program);
+}
+if(argv.v){
+  console.log('PirateLang: '+packageP.version);
+}
