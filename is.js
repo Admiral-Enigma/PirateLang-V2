117cfe6e414ebd284351cbd470d34f072b742cad
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-20 11:13:06 +0200

    Temp fix for the version printing

--- is.js
+++ is.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
-
+var version = '1.2.9';
 var interpreter = require('./core.js');
 
 if(argv.f){
@@ -10,7 +10,7 @@ if(argv.f){
   interpreter.run(program);
 }
 if(argv.v){
-  var packageJson = fs.readFileSync('./package.json', 'utf8');
-  var packageP = JSON.parse(packageJson);
-  console.log('PirateLang: '+packageP.version);
+  //var packageJson = fs.readFileSync('./package.json', 'utf8');
+  //var packageP = JSON.parse(packageJson);
+  console.log('PirateLang: '+version);
 }
